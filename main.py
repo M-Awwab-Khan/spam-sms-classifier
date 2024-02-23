ff8dfd8639cf6843c60b4b4c3ed702dd24f7ff27
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-23 22:07:25 +0500

    ui adjustments

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ def main(page: ft.Page):
 
 
     page.add(
-        ft.Column(
-            [
-                ft.Text(
-                    "SMS/Email Spam Classifier",
-                    size=40,
-                    weight=ft.FontWeight.BOLD,
-                ),
-                ft.TextField(
-                    label="Enter message body",
-                    multiline=True,
-                    min_lines=1,
-                    max_lines=8,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
+        ft.Container(
+            content=ft.Row([
+                        ft.Column(
+                            [
+                                ft.Text(
+                                    "SMS/Email Spam Classifier",
+                                    size=40,
+                                    weight=ft.FontWeight.BOLD,
+                                ),
+                                ft.TextField(
+                                    label="Enter message body",
+                                    multiline=True,
+                                    min_lines=1,
+                                    max_lines=15,
+                                ),
+                            ],
+                            width=1000, alignment=ft.MainAxisAlignment.CENTER
+                        )
+                    ]),
+                    padding=50
+        
         )
+        
     )
 
 ft.app(target=main)
