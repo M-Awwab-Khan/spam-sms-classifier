57043e88378a69a1f94e8be2fb39aa4eadc1b4ce
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-24 17:53:21 +0500

    classify msg spam or not

---
+++ classify.py
@@ -0,0 +1,10 @@
+import pickle
+from preprocess import preprocess_text
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+def classify(msg):
+    processed_msg = preprocess_text(msg)
+    transformed_msg= tfidf.transform([processed_msg])
+    prediction = model.predict(transformed_msg)[0]
+    return 'Spam' if prediction else 'Not Spam'
